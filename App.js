7204583636429018e64819febad185e81986631b
Flávio Lúcio Naves Júnior <flavio@rhemasystem.com.br>
2020-06-11 21:39:41 -0300

    add index.js to be the principal file

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { render } from 'react-dom';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>App para consumir REST API!</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
